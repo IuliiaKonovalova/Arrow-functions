a13e24288b741f6e055e8a362a6090f4c2619588
Iuliia_Konovalova <JuliiaKonn@protonmail.com>
2021-11-22 06:08:12 +0000

    Solve challenges for arrow function in js

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,41 @@
 
 // Vanilla JavaScript Function
 
+
+function sumNumbers (a, b) {
+    return a + b
+}
+
+console.log(sumNumbers())
+
 // Arrow Function With Parameters
+
+const sumNumbersArrowFunction = (a, b) => {
+    return a + b
+}
+
+console.log(sumNumbersArrowFunction())
 
 // Single Line Arrow Function With Parameters
 
+const sumNumbersArrowFunctionSingLine = (a, b) => a + b
+
+console.log(sumNumbersArrowFunctionSingLine())
+
 // Implicit Returns
 
+const sayHellow = message => console.log(message)
+
+console.log(sayHellow())
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+`    <p>
+        <div>
+            <h1></h1>
+        </div>
+    </p>`
+)
+
+console.log(returnMultipleLines())
